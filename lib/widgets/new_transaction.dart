b2e43f4af9460c614c8bb1fa76f4c067592656df
damjanh <damjan.hudobreznik@gmail.com>
2020-03-02 12:54:31 +0100

    Fix add new transaction input.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
   NewTransaction(this.addNewTransaction);
+
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   void inputSubmit() {
     final title = titleController.text;
@@ -15,7 +21,7 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addNewTransaction(title, amount);
+    widget.addNewTransaction(title, amount);
   }
 
   @override
