51ee7c457536a8f22d3b62fe67e3c6f0d4a1f48b
damjanh <damjan.hudobreznik@gmail.com>
2020-03-02 09:41:25 +0100

    Implement transaction item style.

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,44 @@ class HomeWidget extends StatelessWidget {
             children: <Widget>[
               ...transactions.map((transaction) {
                 return Card(
-                  child: Text(transaction.title),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          transaction.amount.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black54),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.black54,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transaction.title,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            transaction.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 );
               }).toList(),
             ],
