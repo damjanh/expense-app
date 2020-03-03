8c817ab1faf4e65eacabb16226f692412a015b3a
damjanh <damjan.hudobreznik@gmail.com>
2020-03-03 12:10:43 +0100

    Use button for delete if width permits on list items.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -61,12 +61,19 @@ class TransactionList extends StatelessWidget {
                     subtitle: Text(
                       DateFormat.yMMMd().format(transactions[index].date),
                     ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      color: Theme.of(context).errorColor,
-                      onPressed: () =>
-                          deleteTransaction(transactions[index].id),
-                    ),
+                    trailing: MediaQuery.of(context).size.width > 360
+                        ? FlatButton.icon(
+                            onPressed: () =>
+                                deleteTransaction(transactions[index].id),
+                            textColor: Theme.of(context).errorColor,
+                            icon: Icon(Icons.delete),
+                            label: Text('Delete'))
+                        : IconButton(
+                            icon: Icon(Icons.delete),
+                            color: Theme.of(context).errorColor,
+                            onPressed: () =>
+                                deleteTransaction(transactions[index].id),
+                          ),
                   ),
                 );
               },
