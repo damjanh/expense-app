2534579945bdd15fa64adc1603d6b396e8e04cf5
damjanh <damjan.hudobreznik@gmail.com>
2020-03-04 07:36:19 +0100

    Create separate widget for transaction list item.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import '../model/transaction.dart';
+import './transaction_item.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -36,46 +36,7 @@ class TransactionList extends StatelessWidget {
             })
           : ListView.builder(
               itemBuilder: (context, index) {
-                return Card(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 8,
-                    horizontal: 5,
-                  ),
-                  elevation: 5,
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      radius: 30,
-                      child: Padding(
-                        padding: const EdgeInsets.all(6),
-                        child: FittedBox(
-                          child: Text(
-                            '\$${transactions[index].amount}',
-                          ),
-                        ),
-                      ),
-                    ),
-                    title: Text(
-                      transactions[index].title,
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                    ),
-                    trailing: MediaQuery.of(context).size.width > 360
-                        ? FlatButton.icon(
-                            onPressed: () =>
-                                deleteTransaction(transactions[index].id),
-                            textColor: Theme.of(context).errorColor,
-                            icon: const Icon(Icons.delete),
-                            label: const Text('Delete'))
-                        : IconButton(
-                            icon: const Icon(Icons.delete),
-                            color: Theme.of(context).errorColor,
-                            onPressed: () =>
-                                deleteTransaction(transactions[index].id),
-                          ),
-                  ),
-                );
+                return TransactionItem(transaction: transactions[index], deleteTransaction: deleteTransaction);
               },
               itemCount: transactions.length,
             ),
